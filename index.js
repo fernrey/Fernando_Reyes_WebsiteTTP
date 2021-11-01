0b534dd104c0bace2553b5b2e741a82b52b0d06c
fernrey <59709131+fernrey@users.noreply.github.com>
2021-11-01 01:27:13 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function realtimeClock () {
+
+	var rtClock = new Date ();
+	var hours = rtClock.getHours();
+	var min = rtClock.getMinutes();
+	var sec = rtClock.getSeconds();
+
+	
+	var amPm = (hours < 12) ? "AM" : "PM";
+	hours = hours > 12 ? hours - 12 : hours;
+	if (hours  == 0){
+		hours = 12;
+	}
+
+	hours = ("0" + hours).slice(-2);
+	min = ("0" + min ).slice(-2);
+	sec = ("0" + sec).slice(-2);
+	document.getElementById('clock').innerHTML = 
+	hours + " : " + min + " : " + sec + " " + amPm;
+	var t = setTimeout(realtimeClock,500);
+
+}
